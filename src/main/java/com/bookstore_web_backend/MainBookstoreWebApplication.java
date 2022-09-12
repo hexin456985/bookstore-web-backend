021be54c000be7c5e504568b18f4b9cc14336043
hexin456985 <3180100141@zju.edu.cn>
2022-09-12 19:09:19 +0800

    first commit

---
+++ src/main/java/com/bookstore_web_backend/MainBookstoreWebApplication.java
@@ -0,0 +1,11 @@
+package com.bookstore_web_backend;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class MainBookstoreWebApplication {
+    public static void main(String[] args){
+        SpringApplication.run(MainBookstoreWebApplication.class,args);
+    }
+}
